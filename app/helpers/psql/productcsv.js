e6c8ba8569df878b6176712048b366b43a539721
Irene Shao <ireneshao@gmail.com>
2019-02-28 18:51:03 -0800

    Implement schema for psql and cassandra

---
+++ app/helpers/psql/productcsv.js
@@ -0,0 +1,11 @@
+const faker = require('faker');
+const fs = require('file-system');
+
+let id = 1;
+
+function make10MListings(fileSuffix) {
+}
+
+for (let i = 1; i <= 10; i += 1) {
+  make10MListings(i);
+}
