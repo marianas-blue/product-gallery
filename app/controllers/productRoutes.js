c562a06192b571dad9619665f14df0d0a23f0af8
Irene Shao <ireneshao@gmail.com>
2019-03-06 17:47:20 -0800

    Add all routes

--- app/controllers/productRoutes.js
+++ app/controllers/productRoutes.js
@@ -14,7 +14,56 @@ router.get('/:id', async (req, res) => {
   const product = await Products.getOne(id);
   product.rows[0].primary_image = product.rows[0].link;
   product.rows[0].secondary_image = product.rows[1].link;
+  delete product.rows[0].link;
   res.send(product.rows[0]);
+});
+
+router.post('', async (req, res) => {
+  const {
+    name,
+    category,
+    manufacturer,
+    primary_image,
+    secondary_image,
+    price,
+    is_prime,
+    description,
+  } = req.body;
+
+  const id = await Products.postOne(name, category, manufacturer, primary_image, secondary_image, price, is_prime, description);
+  res.send({ id });
+});
+
+router.post('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { problem_section, issue, comments } = req.body;
+  await Products.postReport(id, problem_section, issue, comments);
+  res.send({ id });
+});
+
+router.get('/:seller_id', async (req, res) => {
+  const { seller_id } = req.params;
+  const product = await Products.getSeller(seller_id);
+  res.send(product);
+});
+
+router.get('/:product_name', async (req, res) => {
+  const { product_name } = req.params;
+  const product = await Products.getAllSellers(product_name);
+  res.send(product);
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  await Products.updateOne(id, data);
+  res.send(200);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  await Products.deleteOne(id);
+  res.send(200);
 });
 
 module.exports = router;
--- app/models/products.js
+++ app/models/products.js
@@ -21,7 +21,77 @@ const getOne = async (id) => {
   return res;
 };
 
+const postOne = async (name, category, manufacturer, primaryImage, secondaryImage, price, isPrime, description) => {
+  const client = new Client(database);
+  await client.connect();
+  const result = await client.query(`INSERT INTO products (name, category, manufacturer, price, is_prime, description) VALUES ('${name}', '${category}', '${manufacturer}', ${price}, ${isPrime}, '${description}') RETURNING id`);
+  const { id } = result.rows[0];
+  await client.query(`INSERT INTO images (link, product_id) VALUES ('${primaryImage}', ${id})`);
+  await client.query(`INSERT INTO images (link, product_id) VALUES ('${secondaryImage}', ${id})`);
+  client.end();
+  return id;
+};
+
+const postReport = async (id, problemSection, issue, comments) => {
+  const client = new Client(database);
+  await client.connect();
+  await client.query(`INSERT INTO reports (product_id, problem_section, issue, comments) VALUES ('${id}', '${problemSection}', '${issue}', '${comments}')`);
+  client.end();
+};
+
+const getSeller = async (id) => {
+  const client = new Client(database);
+  await client.connect();
+  const res = await client.query(`SELECT * FROM sellers WHERE id=${id}`);
+  client.end();
+  return res;
+};
+
+const getAllSellers = async (name) => {
+  const client = new Client(database);
+  await client.connect();
+  const res = await client.query(`SELECT * FROM products WHERE name=${name}`);
+  client.end();
+  return res;
+};
+
+const updateOne = async (id, data) => {
+  const {
+    name,
+    category,
+    manufacturer,
+    primary_image,
+    secondary_image,
+    price,
+    is_prime,
+    description,
+  } = data;
+  const client = new Client(database);
+  await client.connect();
+  await client.query(`UPDATE products SET name='${name}', category='${category}', manufacturer='${manufacturer}', price=${price}, is_prime=${is_prime}, description='${description}' WHERE id=${id}`);
+  const result = await client.query(`SELECT id FROM images WHERE product_id=${id}`);
+  const idOne = result.rows[1].id;
+  const idTwo = result.rows[0].id;
+  await client.query(`UPDATE images SET link='${primary_image}' WHERE product_id=${idOne}`);
+  await client.query(`UPDATE images SET link='${secondary_image}' WHERE product_id=${idTwo}`);
+  client.end();
+};
+
+const deleteOne = async (id) => {
+  const client = new Client(database);
+  await client.connect();
+  await client.query(`DELETE FROM images WHERE product_id=${id}`);
+  await client.query(`DELETE FROM products WHERE id=${id}`);
+  client.end();
+};
+
 module.exports = {
   getAll,
   getOne,
+  postOne,
+  postReport,
+  getSeller,
+  getAllSellers,
+  updateOne,
+  deleteOne,
 };
